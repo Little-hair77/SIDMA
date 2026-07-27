2b997164a5b8bc12a1da181073edd5061f1733cb
Pablo Henrique <pablo.henriquecba1@gmail.com>
2026-07-26 23:16:33 -0300

    feat: "Integrating a mock test for structuring purposes"

---
+++ backend/analises/views.py
@@ -0,0 +1,23 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import random
+
+@csrf_exempt
+def diagnosticar_leite(request):
+    if request.method == 'POST' and request.FILES.get('imagem'):
+        # Recebe o arquivo enviado pelo Flutter
+        imagem_recebida = request.FILES['imagem']
+
+        # Simulação mock (enquanto o modelo não está pronto)
+        resultado_ia = random.choice(['Sem indícios de mastite', 'Possível presença de mastite', 'Necessária avaliação adicional'])
+        confianca = round(random.uniform(70.0, 99.0), 2)
+
+        # Posteriormente, esse bloco salvará a referência da imagem e o resultado no PostgreSQL
+        return JsonResponse({
+            'status': 'sucesso',
+            'resultado': resultado_ia,
+            'confiança': f"{confianca}%",
+            'mensagem': 'Análise processada com sucesso (Simulação).'
+        })
+    return JsonResponse({'status': 'erro', 'mensagem': 'Requisição Inválida.'}, status=400)
+    
