274d7148f8d306b238e09cd4451e4567a6b00263
Pablo Henrique <pablo.henriquecba1@gmail.com>
2026-08-13 20:45:23 -0300

    feat: integration of account URLs

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
 from . import views
 
+urlpatterns = [
+    path('auth/google/', views.google_login, name='google_login'),
+    path('auth/registrar/', views.registrar_usuario, name='registrar_usuario'),
+    path('auth/login/', views.login_usuario, name='login_usuario'),
+]
