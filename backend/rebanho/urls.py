0f58ee13005f6337b3990b8717c4c3ac5d7741e8
Pablo Henrique <pablo.henriquecba1@gmail.com>
2026-08-14 00:34:49 -0300

    feat: integration of rebanho app URLs

--- backend/rebanho/urls.py
+++ backend/rebanho/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    path('animais/', views.animais, name='animais'),
+    path('animais/<int:animal_id>/', views.animal_detalhes, name='animal_detalhes'),
+]
