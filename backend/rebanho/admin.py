ef5369f431dc5cf352ee319138c1c87a76d55441
Pablo Henrique <pablo.henriquecba1@gmail.com>
2026-08-14 00:39:27 -0300

    feat: create admin from the rebanho App

--- backend/rebanho/admin.py
+++ backend/rebanho/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Animal
 
 # Register your models here.
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'brinco', 'usuario', 'criado_em')
+    search_fields = ('brinco', 'nome')
