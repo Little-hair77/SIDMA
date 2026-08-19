ebc6cea8b0bd8c5c76eb14a5f909ca34d24099ae
Pablo Henrique <pablo.henriquecba1@gmail.com>
2026-08-19 04:35:32 -0300

    feat: update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)%@wog$c7c9ht!_$9na2c146nbn!wglr#9zu@ph)h+ex6s51k2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '10.0.2.2']
 
 
 # Application definition
