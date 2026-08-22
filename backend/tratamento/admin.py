0a980f46dd6fec3e2b4c0b3cdacd68c2e3477d8b
Pablo Henrique <pablo.henriquecba1@gmail.com>
2026-08-21 23:35:32 -0300

    feat: models "tratamento"

--- backend/tratamento/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Tratamento
-
-
-@admin.register(Tratamento)
-class TratamentoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'animal', 'medicamento', 'data_inicio', 'data_fim_carencia')
-    list_filter = ('data_fim_carencia',)
--- backend/tratamento/models.py
+++ backend/tratamento/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Tratamento (models.Model):
+    """Registra um período de tratamento (ex: antibiótico) e a 
+    data até quando o leite deve ficar em carência"""
+    animal = models.ForeignKey('rebanho.Animal', on_delete=models.CASCADE, related_name='tratamentos')
+    medicamento = models.CharField(max_length=100, blank=True)
+    data_inicio = models.DateField()
+    data_fim_carencia = models.DateField(help_text="Data até quando o leite não deve ser misturado ao tanque")
+    observacoes = models.TextField(blank=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-data_fim_carencia']
+        verbose_name = 'Tratamento'
+        verbose_name_plural = 'Tratamentos'
+
+    def __str__(self):
+        return f"{self.animal.brinco} - {self.medicamento} (até {self.data_fim_carencia:%d/%m/%Y})"
