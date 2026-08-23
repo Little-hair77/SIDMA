3f299af2ed862e9921bb89dca082d2dd3cef7c1b
Pablo Henrique <pablo.henriquecba1@gmail.com>
2026-08-23 17:23:33 -0300

    feat: integration admin "tratamento"

---
+++ backend/tratamento/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Tratamento
+
+@admin.register(Tratamento)
+class TratamentoAdmin(admin.ModelAdmin):
+    list_display('id', 'animal', 'medicamento', 'data_inicio', 'data_fim_carencia')
+    list_filter('data_fim_carencia')
