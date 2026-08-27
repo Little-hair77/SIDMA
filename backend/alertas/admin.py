912c4e01fda48ffe5ee0f4537b420d43e9afa749
Pablo Henrique <pablo.henriquecba1@gmail.com>
2026-08-26 23:52:18 -0300

    feat: integration admin for app "alertas"

--- backend/alertas/admin.py
+++ backend/alertas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Alerta
 
 # Register your models here.
+@admin.register(Alerta)
+class AlertaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'animal', 'tipo', 'ativo', 'criado_em', 'resolvido_em')
+    list_filter = ('tipo', 'ativo')
